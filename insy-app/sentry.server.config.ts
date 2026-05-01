3b054d4f009da30b07f92795b3928e407a29988c
kherbouchehamid23-cpu <kherbouche.hamid23@gmail.com>
2026-05-01 14:05:33 +0000

    feat(core): architecture production-ready avec Auth.js, Rate Limiting (Upstash) et Monitoring (Sentry)

---
+++ insy-app/sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://6550534fba82ddb28f1d1b014fd365f1@o4511314997346304.ingest.de.sentry.io/4511315004162128",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
